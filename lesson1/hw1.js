94b73c795955989be46b9ebac51fec24f5cd4df8
BodiaVegas <BrychkaBh@gmail.com>
2023-08-16 17:40:55 +0300

    hw1 added

---
+++ lesson1/hw1.js
@@ -0,0 +1,71 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua)
+
+let one = 1;
+console.log(one)
+
+let onehun = 100;
+console.log(onehun);
+
+let nine = -999;
+console.log(nine);
+
+let a123 = 123;
+console.log(a123);
+
+let p = 3.1415926535;
+console.log(p);
+
+let two7= 2.7;
+console.log(two7);
+
+let sixteen = 16;
+console.log(sixteen);
+
+let tr=true;
+console.log(tr);
+
+let fl=false;
+console.log(fl);
+
+let firstName = 'Bohdan';
+let middleName = 'Bohdanovych';
+let lastName = 'Brychka';
+let nameRes= firstName+' '+middleName+' '+lastName;
+console.log(nameRes)
+
+let nameres2= `${firstName} ${middleName} ${lastName}`
+console.log(nameres2)
+
+let a = 100; console.log(typeof a);
+let b = '100'; console.log(typeof b);
+let c = true; console.log(c);
+
+let name = prompt ('enter your name, please');
+let middlename = prompt ('enter your middle name, please')
+let surname = prompt('enter your surname please');
+let result = name +' '+middlename+' '+surname
+console.log(result);
+
+
+
---
+++ lesson1/l1.js
@@ -0,0 +1,9 @@
+// alert ('dfgvhbjnkm')
+let a1=130;
+console.log (a1);
+
+
+
+console.log (30+50);
+let b = 'dfghjk';
+console.log (b);
